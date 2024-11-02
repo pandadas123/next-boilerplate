abf8ab7b18d45f225a7ea188a70d07062e1a6e66
pandadas123 <yangziyun02@gmail.com>
2024-11-02 19:27:17 +0800

    fix: tran

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,11 +1,11 @@
 import type { LocalePrefix } from 'node_modules/next-intl/dist/types/src/routing/types';
 
-const localePrefix: LocalePrefix = 'as-needed';
+const localePrefix: LocalePrefix = 'as-needed' as LocalePrefix;
 
 // FIXME: Update this configuration file based on your project information
 export const AppConfig = {
   name: 'Nextjs Starter',
-  locales: ['en', 'fr'],
+  locales: ['en', 'fr', 'zh', 'vh'],
   defaultLocale: 'en',
   localePrefix,
 };
